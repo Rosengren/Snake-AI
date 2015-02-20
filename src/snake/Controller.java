54f32a3a6e83052dab0ce2625df6c624039892e3
Rosengren <kev.rosengren@gmail.com>
2015-02-20 09:33:08 -0500

    refactored Controller

--- src/snake/Controller.java
+++ src/snake/Controller.java
@@ -7,6 +7,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -15,7 +16,6 @@ public class Controller implements ActionListener {
     private static final int DELAY = 50;
     private Queue<Direction> queue;
 
-    private Timer timer;
     private Model model;
     private TAdapter keyPress;
 
@@ -37,17 +37,15 @@ public class Controller implements ActionListener {
         setTimer();
     }
 
+
     private void setTimer() {
-        timer = new Timer(DELAY, this);
+        Timer timer = new Timer(DELAY, this);
         timer.start();
     }
 
 
     private void playAI(AIContext ai) {
-        Direction[] moves = model.runAI(ai);
-        for (Direction dir : moves) {
-            queue.add(dir);
-        }
+        queue.addAll(Arrays.asList(model.runAI(ai)));
     }
 
 
@@ -60,51 +58,45 @@ public class Controller implements ActionListener {
             switch(key) {
                 case KeyEvent.VK_UP:
                     model.moveSnake(Direction.UP);
-                    model.update();
                     break;
                 case KeyEvent.VK_DOWN:
                     model.moveSnake(Direction.DOWN);
-                    model.update();
                     break;
                 case KeyEvent.VK_LEFT:
                     model.moveSnake(Direction.LEFT);
-
-                    model.update();
                     break;
                 case KeyEvent.VK_RIGHT:
                     model.moveSnake(Direction.RIGHT);
-
-                    model.update();
                     break;
                 case KeyEvent.VK_ENTER:
                     model.restartGame();
-                    break;
+                    return;
                 case KeyEvent.VK_SPACE:
                     System.exit(0);
                     break;
                 case KeyEvent.VK_ESCAPE:
                     model.pauseGame();
-                    break;
+                    return;
                 case KeyEvent.VK_1:
                     playAI(new AIContext(new BreadthFirstSearch()));
-                    break;
+                    return;
                 case KeyEvent.VK_2:
                     playAI(new AIContext(new DepthFirstSearch()));
-                    break;
+                    return;
                 case KeyEvent.VK_3:
                     playAI(new AIContext(new AStarTraversal()));
-                    break;
+                    return;
                 default:
                     break;
             }
-
+            model.update();
         }
     }
 
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        // TODO Auto-generated method stub
+
         if (!queue.isEmpty()) {
             model.moveSnake(queue.remove());
             model.update();
